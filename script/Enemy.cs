ea80dc6b9107c75605ff77abd674f82ef61c9208
CygnusDownfall <phapsuhacam112@gmail.com>
2022-07-27 20:04:10 +0700

    27/07/2022

--- script/Enemy.cs
+++ script/Enemy.cs
@@ -4,24 +4,38 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    // thao t�c 
     public GameObject HPcurrnentUI;
+    public character character;
+    public Animator Animator;
+
+    //thu?c t�nh ch�nh 
     public float HP, maxHP,  atk;
     public float rangeAttack=2;
     public kindofenemy kindenemy;
-    public bool attackstate = false;
-    public character character;
+   
+    
     Rigidbody2D rigidbody2d;
     private void Update()
     {
+        
         if (HP != maxHP)
         {
+            if (HP <= 0)
+            {
+                updateHPUI(5);
+                OnDie();
+            }else
             updateHPUI((maxHP - HP) * 5 / maxHP);
-
+            
         }
-        
+       //attack();
 
     }
+    void OnDie()
+    {
 
+    }
     private void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
@@ -35,19 +49,21 @@ public class Enemy : MonoBehaviour
     }
     public IEnumerator attack()
     {
-        while (attackstate)
+        while(true)
         {
 
             kindenemy.movetoplayer(character.gameObject, rigidbody2d, transform);
             if (Mathf.Abs(character.transform.position.x - transform.position.x) <= rangeAttack)
             {
+                
+
                 //chay animation attack
 
                 //tinh dmg
                 character.takedmgEvent(atk);
             }
-            
-            //Debug.Log("cccc");
+            //StartCoroutine( impactskill.delay(1));
+           
             yield return new WaitForSeconds(1);
         }
 
@@ -61,12 +77,12 @@ public class Enemy : MonoBehaviour
         {
 
 
-            attackstate = true;
+            //attackstate = true;
 
             //play animation attack 
 
 
-            StartCoroutine(attack());
+            StartCoroutine("attack");
 
 
 
@@ -77,9 +93,9 @@ public class Enemy : MonoBehaviour
     {
         if (collision.tag == "Player")
         {
-            attackstate = false;
-            // Debug.Log("gfgfh");
-            StopCoroutine(attack());
+            //attackstate = false;
+           
+            StopCoroutine("attack");
         }
     }
 
@@ -91,6 +107,7 @@ public class Enemy : MonoBehaviour
 
     void updateHPUI(float X)
     {
+
         HPcurrnentUI.transform.localScale =new Vector3(X,1,1);
         if (HP < maxHP*70 / 100 )
         {
